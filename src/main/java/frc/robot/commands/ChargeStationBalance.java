a9ae5d99fa3d8b383b2a3aa6b4c73409f3a9459f
UniquelyBad <jeremysgao@gmail.com>
2023-01-24 18:32:08 -0500

    fixed constants and variables, isFinished() and made seperate command for driving onto charge station

--- src/main/java/frc/robot/commands/ChargeStationBalance.java
+++ src/main/java/frc/robot/commands/ChargeStationBalance.java
@@ -12,22 +12,16 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class ChargeStationBalance extends CommandBase {
 
-  //private final Rotation2d m_rampAngle = Rotation2d.fromDegrees(-10); //useful if we are going to automate going on the ramp
-  private final Rotation2d m_balancedError = Rotation2d.fromDegrees(2.5); //error for what counts as balanced
-  private final double m_balancedDegrees = 0;
-  private final double m_balanceKP = 0.02; //change to control how fast robot drives during balancing
-  private final double m_maxMPS = 0.75;
-  private final double m_balanceSeconds = 1; //how many seconds the robot has to be balanced before stopping
+  private final Rotation2d BALANCED_ERROR = Rotation2d.fromDegrees(2.5); //error for what counts as balanced
+  private final double BALANCED_DEGREES = 0;
+  private final double BALANCE_KP = 0.02; //change to control how fast robot drives during balancing
+  private final double MAX_MPS = 0.75;
+  private final double BALANCE_SECONDS = 1; //how many seconds the robot has to be balanced before stopping
 
   private DriveTrain m_driveTrain;
-  private enum m_stages {
-    GROUND,
-    CHARGESTATION
-  }
-  private m_stages m_stage = m_stages.GROUND;
-  private Boolean m_balanced = false;
+  private Boolean m_balanced;
   private final Timer m_timer = new Timer();
-  private double m_curTime = 0;
+  private double m_curTime;
 
   /** Creates a new ChargeStationBalance. */
   public ChargeStationBalance() {
@@ -40,40 +34,28 @@ public class ChargeStationBalance extends CommandBase {
   public void initialize() {
     m_timer.reset();
     m_timer.start();
+
+    m_balanced = false;
+    m_curTime = 0;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double error = 0;
-    double driveMPS = 0;
-    double currentAngle = m_driveTrain.getPitch().getDegrees();
-
-    // Automated way to drive robot onto charge station from ground as well as balance on charge station
-    // switch (m_stage) {
-    // case GROUND: 
-    //   error = m_rampAngle.getDegrees() - currentAngle;
-    //   driveMPS = error * CHARGESTATION_BALANCED_KP;
-    //
-    // set stage to CHARGESTATION once robot is on ramp
-    // if (error < CHARGESTATION_BALANCE_ERROR.getDegrees()){
-    //   m_stage = m_stages.CHARGESTATION;
-    // }
-    // case CHARGESTATION: 
-
-    // uses difference in current angle and 0 to get speed, balancing while on charge station 
-    error = m_balancedDegrees - currentAngle;
-    driveMPS = error * m_balanceKP;
+    //uses angle of robot to set its speed
+    Rotation2d currentAngle = m_driveTrain.getPitch();
+    Rotation2d error = Rotation2d.fromDegrees(BALANCED_DEGREES - currentAngle.getDegrees());
+    double driveMPS = error.getDegrees() * BALANCE_KP;
 
     // cap max speed at 0.75 meters per second
-    if (Math.abs(driveMPS) > m_maxMPS) {
-      driveMPS = Math.copySign(m_maxMPS, driveMPS);
+    if (Math.abs(driveMPS) > MAX_MPS) {
+      driveMPS = Math.copySign(MAX_MPS, driveMPS);
     }
     
     m_driveTrain.drive(ChassisSpeeds.fromFieldRelativeSpeeds(driveMPS, 0.0, 0.0, m_driveTrain.getHeading()));
     
-    //if balanced, records time when state of being balanced started, if not balanced, sets time to 0
-    if (error <= m_balancedError.getDegrees() && m_stage == m_stages.CHARGESTATION){
+    //if balanced, records time when state of being balanced started, if not balanced, sets curtime to 0
+    if (error.getDegrees() <= BALANCED_ERROR.getDegrees()){
       m_balanced = true;
       if (m_curTime == 0){
       m_curTime = m_timer.get();
@@ -94,12 +76,8 @@ public class ChargeStationBalance extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    //if robot is balanced and it has been for one second, robot ends
-    if (m_balanced = true && m_timer.get() == m_curTime + m_balanceSeconds){
-      return true;
-    } else {
-      return false;
-    }
+    //if robot is balanced and it has been for at least one second, robot ends
+    return (m_balanced && m_timer.get() >= m_curTime + BALANCE_SECONDS);
   }
 }
 
