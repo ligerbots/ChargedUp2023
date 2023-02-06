19d0bc9ec981c8d97e890eefd1623e4e72cffa3f
shadowdragon2805 <lexue28@gmail.com>
2023-02-05 19:25:52 -0500

    fixed indentation

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -32,29 +32,28 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Vision {
     // Values for the Shed in late January
-    private static final double CUSTOM_FIELD_LENGTH = 8.780;    // meters
-    private static final double CUSTOM_FIELD_WIDTH = 6.0;       // meters
-    private static final AprilTagFieldLayout SHED_TAG_FIELD_LAYOUT = 
-            new AprilTagFieldLayout(new ArrayList<AprilTag>() {
-                {
-                    add(constructTag(26, 0, 1.636, 0.865, 0));
-                    add(constructTag(25, 0, 3.24, 0.895, 0));
-                    add(constructTag(24, 1.915, 0, 0.857, 90));
-                    add(constructTag(23, 4.958, 0, 0.845, 90));
-                    add(constructTag(22, 7.763, 0, 0.896, 90));
-                    add(constructTag(21, 8.780, 1.373, 0.895, 180));
-                    add(constructTag(20, 8.780, 2.392, 0.946, 180));
-                }
-            }, CUSTOM_FIELD_LENGTH, CUSTOM_FIELD_WIDTH);
+    private static final double CUSTOM_FIELD_LENGTH = 8.780; // meters
+    private static final double CUSTOM_FIELD_WIDTH = 6.0; // meters
+    private static final AprilTagFieldLayout SHED_TAG_FIELD_LAYOUT = new AprilTagFieldLayout(new ArrayList<AprilTag>() {
+        {
+            add(constructTag(26, 0, 1.636, 0.865, 0));
+            add(constructTag(25, 0, 3.24, 0.895, 0));
+            add(constructTag(24, 1.915, 0, 0.857, 90));
+            add(constructTag(23, 4.958, 0, 0.845, 90));
+            add(constructTag(22, 7.763, 0, 0.896, 90));
+            add(constructTag(21, 8.780, 1.373, 0.895, 180));
+            add(constructTag(20, 8.780, 2.392, 0.946, 180));
+        }
+    }, CUSTOM_FIELD_LENGTH, CUSTOM_FIELD_WIDTH);
 
     private final PhotonCamera m_aprilTagCamera = new PhotonCamera("ApriltagCamera");
     private final AprilTagFieldLayout m_aprilTagFieldLayout;
-    
+
     // Forward B&W camera for Apriltags
     // relative position of the camera on the robot ot the robot center
     private final Transform3d m_robotToAprilTagCam = new Transform3d(
-        new Translation3d(Units.inchesToMeters(31.25 / 2.0), 0.0, Units.inchesToMeters(21.0)),
-        new Rotation3d(0.0, 0.0, 0.0)); 
+            new Translation3d(Units.inchesToMeters(31.25 / 2.0), 0.0, Units.inchesToMeters(21.0)),
+            new Rotation3d(0.0, 0.0, 0.0));
 
     private final PhotonPoseEstimator m_photonPoseEstimator;
 
@@ -62,10 +61,10 @@ public class Vision {
 
     public Vision() {
         // try{
-        // 	m_aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+        // m_aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
         // } catch(IOException e){
-        // 	System.out.println("Unable to load AprilTag layout" + e.getMessage());
-        // 	m_aprilTagFieldLayout = null;
+        // System.out.println("Unable to load AprilTag layout" + e.getMessage());
+        // m_aprilTagFieldLayout = null;
         // }
         m_aprilTagFieldLayout = SHED_TAG_FIELD_LAYOUT;
         System.out.println("Vision is currently using: SHED_TAG_FIELD_LAYOUT");
@@ -84,13 +83,13 @@ public class Vision {
         var targetResult = m_aprilTagCamera.getLatestResult();
         double curImageTimeStamp = targetResult.getTimestampSeconds();
 
-        if (curImageTimeStamp <= m_lastImageTimeStamp) 
+        if (curImageTimeStamp <= m_lastImageTimeStamp)
             return;
 
         m_lastImageTimeStamp = curImageTimeStamp;
 
         SmartDashboard.putBoolean("vision/hasTargets", targetResult.hasTargets());
-        if (!targetResult.hasTargets()) 
+        if (!targetResult.hasTargets())
             return;
 
         // Get the current best target.
@@ -103,7 +102,7 @@ public class Vision {
 
         if (m_aprilTagFieldLayout == null)
             return;
-        
+
         // Estimate the robot pose.
         // If successful, update the odometry using the timestamp of the measurement
         Optional<EstimatedRobotPose> result = getEstimatedGlobalPose(odometry.getEstimatedPosition());
@@ -114,11 +113,11 @@ public class Vision {
             SmartDashboard.putNumber("vision/estimatedPoseX", estimatedPose.getX());
             SmartDashboard.putNumber("vision/estimatedPoseY", estimatedPose.getY());
             SmartDashboard.putNumber("vision/estimatedPoseZ", estimatedPose.getRotation().getAngle());
-        // } else {
-        //     // move it way off the screen to make it disappear
-        //     SmartDashboard.putNumber("vision/estimatedPoseX", 0);
-        //     SmartDashboard.putNumber("vision/estimatedPoseY", 0);
-        //     SmartDashboard.putNumber("vision/estimatedPoseZ", 0);	
+            // } else {
+            // // move it way off the screen to make it disappear
+            // SmartDashboard.putNumber("vision/estimatedPoseX", 0);
+            // SmartDashboard.putNumber("vision/estimatedPoseY", 0);
+            // SmartDashboard.putNumber("vision/estimatedPoseZ", 0);
         }
     }
 
@@ -127,37 +126,37 @@ public class Vision {
         return m_photonPoseEstimator.update();
     }
 
-	public Pose2d getCentralTagPose() { //gets target closets to center of camera
-		var targetResult = m_aprilTagCamera.getLatestResult();
-		int targetID = -1;
-		if (targetResult.hasTargets()) {
-			// Get lists of targets
+    public Pose2d getCentralTagPose() { // gets target closets to center of camera
+        var targetResult = m_aprilTagCamera.getLatestResult();
+        int targetID = -1;
+        if (targetResult.hasTargets()) {
+            // Get lists of targets
             List<PhotonTrackedTarget> targets = targetResult.getTargets();
-            //make a temp holder var for least Y translation
+            // make a temp holder var for least Y translation
             Translation2d translationHolder = new Translation2d(0, 0);
-            for(PhotonTrackedTarget tag: targets){ //for every target in camera
-                //get transformation to target
-                Transform3d tagTransform = tag.getBestCameraToTarget(); 
-                //get translation to target from transformation
+            for (PhotonTrackedTarget tag : targets) { // for every target in camera
+                // get transformation to target
+                Transform3d tagTransform = tag.getBestCameraToTarget();
+                // get translation to target from transformation
                 Translation2d tagTranslation = tagTransform.getTranslation().toTranslation2d();
 
-                //looking for smallest absolute relative to camera Y
-                //if abs Y translation of new tag is less then holder tag, it becomes holder tag
-                if(Math.abs(tagTranslation.getY()) < Math.abs(translationHolder.getY())){
+                // looking for smallest absolute relative to camera Y
+                // if abs Y translation of new tag is less then holder tag, it becomes holder tag
+                if (Math.abs(tagTranslation.getY()) < Math.abs(translationHolder.getY())) {
                     translationHolder = tagTranslation;
-                    targetID = tag.getFiducialId(); //set targetID
+                    targetID = tag.getFiducialId(); // set targetID
                 }
             }
-		}
-		return m_aprilTagFieldLayout.getTagPose(targetID).get().toPose2d();
+        }
+        return m_aprilTagFieldLayout.getTagPose(targetID).get().toPose2d();
 
-	}
+    }
 
-    public Optional<Pose2d> getTagPose(int tagID){
+    public Optional<Pose2d> getTagPose(int tagID) {
         return Optional.of(m_aprilTagFieldLayout.getTagPose(tagID).get().toPose2d());
     }
 
-    private static AprilTag constructTag(int id, double x, double y, double z, double angle){
+    private static AprilTag constructTag(int id, double x, double y, double z, double angle) {
         return new AprilTag(id, new Pose3d(x, y, z, new Rotation3d(0, 0, Math.toRadians(angle))));
     }
 }
