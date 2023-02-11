ff4cb66eae7b07ead57b3d6dd47c75392e7728a4
Jack Wei <jack_weq@icloud.com>
2023-02-11 10:50:21 -0500

    merged from main

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,10 @@
 
 package frc.robot;
 
-<<<<<<< HEAD
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
-=======
->>>>>>> main
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -27,79 +24,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-<<<<<<< HEAD
-  private Command m_autonomousCommand;
-  private SendableChooser<Command> m_chosenTrajectory = new SendableChooser<>();
-  private RobotContainer m_robotContainer;
-  double m_goal = Math.toRadians(90.0);
-  WPI_TalonFX m_motorLeader = new WPI_TalonFX(2, "rio");  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-
-    // if(this.isSimulation()){
-    //   //At-Home Network Debug Only - host the NT server on photonvision and connect to it.
-    //   NetworkTableInstance.getDefault().stopServer();
-    //   // NetworkTableInstance.getDefault().setServer("photonvision.local");
-    //   NetworkTableInstance.getDefault().setServer("127.0.0.1");
-    //   NetworkTableInstance.getDefault().startClient4("MainRobotProgram");
-    // }
-    m_motorLeader.configFactoryDefault();
-		
-		/* Config the sensor used for Primary PID and sensor direction */
-        m_motorLeader.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 
-                                            0,
-				                            0);
-
-		/* Ensure sensor is positive when output is positive */
-		m_motorLeader.setSensorPhase(true);
-    m_motorLeader.setInverted(false);
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
-    m_chosenTrajectory.setDefaultOption("drive_1m", m_robotContainer.getDriveTrain().getTrajectoryFollowingCommand("drive_1m"));
-    m_chosenTrajectory.addOption("drive_and_slide", m_robotContainer.getDriveTrain().getTrajectoryFollowingCommand("drive_and_slide"));
-    m_chosenTrajectory.addOption("drive_and_turn", m_robotContainer.getDriveTrain().getTrajectoryFollowingCommand("drive_and_turn"));
-    m_chosenTrajectory.addOption("Test", m_robotContainer.getDriveTrain().getTrajectoryFollowingCommand("Test"));
-    SmartDashboard.putData("Chosen Trajectory", m_chosenTrajectory);
-    SmartDashboard.putNumber("Arm Goal", Math.toDegrees(m_goal));
-    
-  
-  }
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_chosenTrajectory.getSelected();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-=======
     private Command m_autonomousCommand;
     private SendableChooser<Command> m_chosenTrajectory = new SendableChooser<>();
     private RobotContainer m_robotContainer;
@@ -177,7 +101,6 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.schedule();
         }
->>>>>>> main
     }
 
     /** This function is called periodically during autonomous. */
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,84 +12,67 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import frc.robot.commands.Drive;
-<<<<<<< HEAD
 import frc.robot.commands.SetArmAngle;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Shoulder;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-=======
 import frc.robot.commands.ChargeStationBalance;
 
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Vision;
->>>>>>> main
 
 /**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-<<<<<<< HEAD
 	// The robot's subsystems and commands are defined here...
-	private final DriveTrain m_driveTrain = new DriveTrain();
-	private final XboxController m_controller = new XboxController(0);
+
 	private final Arm m_arm = new Arm();
-=======
-    // The robot's subsystems and commands are defined here...
 
-    private final Vision m_vision = new Vision();
->>>>>>> main
+	private final Vision m_vision = new Vision();
 
-    private final DriveTrain m_driveTrain = new DriveTrain(m_vision);
+	private final DriveTrain m_driveTrain = new DriveTrain(m_vision);
 
-    private final XboxController m_controller = new XboxController(0);
-
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        configureButtonBindings();
-		m_driveTrain.setDefaultCommand(getDriveCommand());
-    }
+	private final XboxController m_controller = new XboxController(0);
 
 	/**
-	 * Use this method to define your button->command mappings. Buttons can be
-	 * created by
-	 * instantiating a {@link GenericHID} or one of its subclasses ({@link
-	 * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-	 * it to a {@link
-	 * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 */
+	public RobotContainer() {
+		configureButtonBindings();
+		m_driveTrain.setDefaultCommand(getDriveCommand());
+	}
+
+	/**
+	 * Use this method to define your button->command mappings. Buttons can be created by instantiating a
+	 * {@link GenericHID} or one of its subclasses ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}),
+	 * and then passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
 	 */
 	private void configureButtonBindings() {
-<<<<<<< HEAD
-		// Back button zeros the gyroscope
-		/*
-		 * new Button(m_controller::getBackButton)
-		 * // No requirements because we don't need to interrupt anything
-		 * .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
-		 */
-		
-		//  JoystickButton xboxYButton = new JoystickButton(m_controller, Constants.XBOX_Y);
-		//  //The angle needs to be calibrated to get the right hight 
-		//  xboxYButton.onTrue(new SetArmAngle(m_arm,0.0));
-			
-		
-
-		// button A
+		// when button A is pressed, toggle field-centric drive mode
 		JoystickButton xboxAButton = new JoystickButton(m_controller, Constants.XBOX_A);
-		// when button A is pressed make a new toggle command to toggle mode
 		xboxAButton.onTrue(new InstantCommand(m_driveTrain::toggleFieldCentric));
 
-		// button X
+		// when button X is pressed, toggle precision (slow) drive mode
 		JoystickButton xboxXButton = new JoystickButton(m_controller, Constants.XBOX_X);
-		// inline command to toggle precision mode when button X is pressed
 		xboxXButton.onTrue(new InstantCommand(m_driveTrain::togglePrecisionMode));
+
+		// when button Y is pressed, attempt to balance on the Charging Station
+		// assumes that the robot is already mostly up on the Station
+		JoystickButton xboxYButton = new JoystickButton(m_controller, Constants.XBOX_Y);
+		xboxYButton.onTrue(new ChargeStationBalance(m_driveTrain));
+
+		// when button Y is pressed, attempt to drive up onto the Charging Station
+		// JoystickButton xboxYButton = new JoystickButton(m_controller, Constants.XBOX_Y);
+		// xboxYButton.onTrue(new ChargeStationDrive());
+
+		// when button START is pressed, reset the robot heading
+		// whichever way the robot is facing becomes the forward direction
+		JoystickButton xboxStartButton = new JoystickButton(m_controller, Constants.XBOX_START);
+		xboxStartButton.onTrue(new InstantCommand(m_driveTrain::resetHeading));
 	}
 
 	public Command getDriveCommand() {
@@ -97,11 +80,8 @@ public class RobotContainer {
 		// Left stick Y axis -> forward and backwards movement
 		// Left stick X axis -> left and right movement
 		// Right stick X axis -> rotation
-		return new Drive(
-				m_driveTrain,
-				() -> -modifyAxis(m_controller.getLeftY()),
-				() -> -modifyAxis(m_controller.getLeftX()),
-				() -> -modifyAxis(m_controller.getRightX()));
+		return new Drive(m_driveTrain, () -> -modifyAxis(m_controller.getLeftY()),
+				() -> -modifyAxis(m_controller.getLeftX()), () -> -modifyAxis(m_controller.getRightX()));
 	}
 
 	private static double deadband(double value, double deadband) {
@@ -124,66 +104,9 @@ public class RobotContainer {
 		value = Math.copySign(value * value, value);
 
 		return value;
-=======
-		// when button A is pressed, toggle field-centric drive mode
-		JoystickButton xboxAButton = new JoystickButton(m_controller, Constants.XBOX_A);
-		xboxAButton.onTrue(new InstantCommand(m_driveTrain::toggleFieldCentric));
-
-		// when button X is pressed, toggle precision (slow) drive mode 
-		JoystickButton xboxXButton = new JoystickButton(m_controller, Constants.XBOX_X);
-		xboxXButton.onTrue(new InstantCommand(m_driveTrain::togglePrecisionMode));
-
-		// when button Y is pressed, attempt to balance on the Charging Station
-		// assumes that the robot is already mostly up on the Station
-		JoystickButton xboxYButton = new JoystickButton(m_controller, Constants.XBOX_Y);
-		xboxYButton.onTrue(new ChargeStationBalance(m_driveTrain));
-
-		// when button Y is pressed, attempt to drive up onto the Charging Station
-		// JoystickButton xboxYButton = new JoystickButton(m_controller, Constants.XBOX_Y);
-		// xboxYButton.onTrue(new ChargeStationDrive());
-
-		// when button START is pressed, reset the robot heading
-		// whichever way the robot is facing becomes the forward direction
-		JoystickButton xboxStartButton = new JoystickButton(m_controller, Constants.XBOX_START);
-		xboxStartButton.onTrue(new InstantCommand(m_driveTrain::resetHeading));
->>>>>>> main
 	}
 
-    public Command getDriveCommand() {
-        // The controls are for field-oriented driving:
-        // Left stick Y axis -> forward and backwards movement
-        // Left stick X axis -> left and right movement
-        // Right stick X axis -> rotation
-        return new Drive(
-                m_driveTrain,
-                () -> -modifyAxis(m_controller.getLeftY()),
-                () -> -modifyAxis(m_controller.getLeftX()),
-                () -> -modifyAxis(m_controller.getRightX()));
-    }
-
-    private static double deadband(double value, double deadband) {
-        if (Math.abs(value) > deadband) {
-            if (value > 0.0) {
-                return (value - deadband) / (1.0 - deadband);
-            } else {
-                return (value + deadband) / (1.0 - deadband);
-            }
-        } else {
-            return 0.0;
-        }
-    }
-
-    private static double modifyAxis(double value) {
-        // Deadband
-        value = deadband(value, 0.05);
-
-        // Square the axis
-        value = Math.copySign(value * value, value);
-
-        return value;
-    }
-
-    public DriveTrain getDriveTrain() {
-        return m_driveTrain;
-    }
+	public DriveTrain getDriveTrain() {
+		return m_driveTrain;
+	}
 }
