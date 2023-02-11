71a16ad0aa97024462eab34c093e0ae38235b4cc
Paul Rensing <prensing@gmail.com>
2023-02-10 19:33:58 -0500

    swerve offset for new robot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,22 +34,22 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(168.1);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(87.5);
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(20.7);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.5);
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(358.2);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(77.3);
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(91.4);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(110.2);
 
     public static final double X_PID_CONTROLLER_P = 0.2;
     public static final double Y_PID_CONTROLLER_P = 0.2;
