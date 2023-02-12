8c817904a7ee39d044986757715e0ae127d6e161
Paul Rensing <prensing@ligerbots.org>
2023-02-12 18:04:14 -0500

    fix indentation

--- src/main/java/frc/robot/commands/AutoFollowTrajectory.java
+++ src/main/java/frc/robot/commands/AutoFollowTrajectory.java
@@ -17,66 +17,66 @@ import frc.robot.subsystems.DriveTrain;
 
 public class AutoFollowTrajectory extends CommandBase implements AutoCommandInterface {
 
-	private final DriveTrain m_driveTrain;
-	private final PathPlannerTrajectory m_blueTrajectory, m_redTrajectory;
-	private Command m_trajFollowCommand;
+    private final DriveTrain m_driveTrain;
+    private final PathPlannerTrajectory m_blueTrajectory, m_redTrajectory;
+    private Command m_trajFollowCommand;
 
-	/** Creates a new AutoFollowTrajectory. */
-	public AutoFollowTrajectory(DriveTrain driveTrain, String trajectoryName) {
-		// Use addRequirements() here to declare subsystem dependencies.
-		m_driveTrain = driveTrain;
-		m_blueTrajectory = PathPlanner.loadPath(trajectoryName + "_blue", Constants.TRAJ_MAX_VEL,
-				Constants.TRAJ_MAX_ACC);
-		m_redTrajectory = PathPlanner.loadPath(trajectoryName + "_red", Constants.TRAJ_MAX_VEL,
-				Constants.TRAJ_MAX_ACC);
-	}
+    /** Creates a new AutoFollowTrajectory. */
+    public AutoFollowTrajectory(DriveTrain driveTrain, String trajectoryName) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        m_driveTrain = driveTrain;
+        m_blueTrajectory = PathPlanner.loadPath(trajectoryName + "_blue", Constants.TRAJ_MAX_VEL,
+                Constants.TRAJ_MAX_ACC);
+        m_redTrajectory = PathPlanner.loadPath(trajectoryName + "_red", Constants.TRAJ_MAX_VEL,
+                Constants.TRAJ_MAX_ACC);
+    }
 
-	// Called when the command is initially scheduled.
-	@Override
-	public void initialize() {
-		PathPlannerTrajectory curTraj;
-		if (DriverStation.getAlliance() == Alliance.Red)
-			curTraj = m_redTrajectory;
-		else
-			curTraj = m_blueTrajectory;
-		m_trajFollowCommand = m_driveTrain.makeFollowTrajectoryCommand(curTraj);
-		CommandScheduler.getInstance().schedule(m_trajFollowCommand);
-	}
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        PathPlannerTrajectory curTraj;
+        if (DriverStation.getAlliance() == Alliance.Red)
+            curTraj = m_redTrajectory;
+        else
+            curTraj = m_blueTrajectory;
+        m_trajFollowCommand = m_driveTrain.makeFollowTrajectoryCommand(curTraj);
+        CommandScheduler.getInstance().schedule(m_trajFollowCommand);
+    }
 
-	// Called every time the scheduler runs while the command is scheduled.
-	@Override
-	public void execute() {
-	}
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+    }
 
-	// Called once the command ends or is interrupted.
-	@Override
-	public void end(boolean interrupted) {
-		if (interrupted) {
-			m_trajFollowCommand.cancel();
-		}
-		m_trajFollowCommand = null;
-	}
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        if (interrupted) {
+            m_trajFollowCommand.cancel();
+        }
+        m_trajFollowCommand = null;
+    }
 
-	// Returns true when the command should end.
-	@Override
-	public boolean isFinished() {
-		return m_trajFollowCommand == null || !m_trajFollowCommand.isScheduled();
-	}
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return m_trajFollowCommand == null || !m_trajFollowCommand.isScheduled();
+    }
 
-	@Override
-	public Pose2d getInitialPose() {
-		// TODO Auto-generated method stub
-		if (DriverStation.getAlliance() == Alliance.Red)
-			return m_redTrajectory.getInitialPose();
-		else
-			return m_blueTrajectory.getInitialPose();
-	}
+    @Override
+    public Pose2d getInitialPose() {
+        // TODO Auto-generated method stub
+        if (DriverStation.getAlliance() == Alliance.Red)
+            return m_redTrajectory.getInitialPose();
+        else
+            return m_blueTrajectory.getInitialPose();
+    }
 
-	@Override
-	public void plotTrajectory(TrajectoryPlotter plotter) {
-		if (DriverStation.getAlliance() == Alliance.Red)
-			plotter.plotTrajectory(m_redTrajectory);
-		else
-			plotter.plotTrajectory(m_blueTrajectory);
-	}
+    @Override
+    public void plotTrajectory(TrajectoryPlotter plotter) {
+        if (DriverStation.getAlliance() == Alliance.Red)
+            plotter.plotTrajectory(m_redTrajectory);
+        else
+            plotter.plotTrajectory(m_blueTrajectory);
+    }
 }
