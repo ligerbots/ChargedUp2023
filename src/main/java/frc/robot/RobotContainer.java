b0a2739e34df4c03697453ec0afc7448576012f1
Paul Rensing <prensing@ligerbots.org>
2023-02-15 20:48:59 -0500

    Reorg Constants, move them into relevant classes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,13 +28,28 @@ import frc.robot.subsystems.Vision;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems and commands are defined here...
+    // Xbox button mapping
+    private static final int XBOX_A = 1;
+    private static final int XBOX_B = 2;
+    private static final int XBOX_X = 3;
+    private static final int XBOX_Y = 4;
 
-    private final Vision m_vision = new Vision();
+    // left and right bumpers
+    // private static final int XBOX_LB = 5;
+    // private static final int XBOX_RB = 6;
+    
+    // private static final int XBOX_BACK = 7;
+    private static final int XBOX_START = 8;
 
-    private final DriveTrain m_driveTrain = new DriveTrain(m_vision);
+    // joy stick button
+    // private static final int XBOX_JL = 9;
+    // private static final int XBOX_JR = 10;
 
     private final XboxController m_controller = new XboxController(0);
+
+    // The robot's subsystems and commands are defined here...
+    private final Vision m_vision = new Vision();
+    private final DriveTrain m_driveTrain = new DriveTrain(m_vision);
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -54,24 +69,24 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         // when button A is pressed, toggle field-centric drive mode
-        JoystickButton xboxAButton = new JoystickButton(m_controller, Constants.XBOX_A);
+        JoystickButton xboxAButton = new JoystickButton(m_controller, XBOX_A);
         xboxAButton.onTrue(new InstantCommand(m_driveTrain::toggleFieldCentric));
 
         // when button B is pressed, lock wheels
-        JoystickButton xboxBButton = new JoystickButton(m_controller, Constants.XBOX_B);
+        JoystickButton xboxBButton = new JoystickButton(m_controller, XBOX_B);
         xboxBButton.onTrue(new InstantCommand(m_driveTrain::lockWheels, m_driveTrain));
 
         // when button X is pressed, toggle precision (slow) drive mode
-        JoystickButton xboxXButton = new JoystickButton(m_controller, Constants.XBOX_X);
+        JoystickButton xboxXButton = new JoystickButton(m_controller, XBOX_X);
         xboxXButton.onTrue(new InstantCommand(m_driveTrain::togglePrecisionMode));
 
         // when button START is pressed, reset the robot heading
         // whichever way the robot is facing becomes the forward direction
-        JoystickButton xboxStartButton = new JoystickButton(m_controller, Constants.XBOX_START);
+        JoystickButton xboxStartButton = new JoystickButton(m_controller, XBOX_START);
         xboxStartButton.onTrue(new InstantCommand(m_driveTrain::resetHeading));
 
         // ---- TESTING  ----
-        JoystickButton xboxYButton = new JoystickButton(m_controller, Constants.XBOX_Y);
+        JoystickButton xboxYButton = new JoystickButton(m_controller, XBOX_Y);
 
         // testing if the command works by passing in a position, we need more buttons for all 11
         xboxYButton.onTrue(new TagPositionDrive(m_driveTrain, m_vision, Constants.Position.LEFT_TOP));
@@ -81,18 +96,18 @@ public class RobotContainer {
         // xboxYButton.onTrue(new ChargeStationBalance(m_driveTrain));
 
         // when button Y is pressed, attempt to drive up onto the Charging Station
-        // JoystickButton xboxYButton = new JoystickButton(m_controller, Constants.XBOX_Y);
+        // JoystickButton xboxYButton = new JoystickButton(m_controller, XBOX_Y);
         // xboxYButton.onTrue(new ChargeStationDrive());
 
         /* //Commented out for now
         // button B
-        JoystickButton xboxBButton = new JoystickButton(m_controller, Constants.XBOX_B);
+        JoystickButton xboxBButton = new JoystickButton(m_controller, XBOX_B);
         // inline command to create trajectory from robot pose to middle of the best apriltag
         xboxBButton.onTrue(new ProxyCommand(() -> m_driveTrain.trajectoryToPose(m_driveTrain.getTagRobotPose(false, false))));
         //need a proxy so command is not created before button pressed
         
         // button Y
-        JoystickButton xboxYButton = new JoystickButton(m_controller, Constants.XBOX_Y);
+        JoystickButton xboxYButton = new JoystickButton(m_controller, XBOX_Y);
         // inline command to create trajectory from robot pose to right of the best apriltag
         xboxYButton.onTrue(new ProxyCommand(() -> m_driveTrain.trajectoryToPose(m_driveTrain.getTagRobotPose(false, true))));
         */
