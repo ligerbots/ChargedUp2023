4ca1957783640b14dcfc6a33082ae935b5934e7e
Paul Rensing <prensing@ligerbots.org>
2023-02-20 11:26:40 -0500

    Formatting and TODO comments

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -17,50 +17,55 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Claw extends SubsystemBase {
-	/** Creates a new Claw. */
+    private Compressor m_phCompressor = new Compressor(Constants.PNEUMATIC_HUB_PORT, PneumaticsModuleType.REVPH);
+    // TODO: check what is the module number
+    private DoubleSolenoid m_clawSolenoid = new DoubleSolenoid(Constants.DOUBLE_SOLENOID_MODULE_NUMBER, PneumaticsModuleType.REVPH, Constants.DOUBLE_SOLENOID_FORWARD_CHANNEL, Constants.DOUBLE_SOLENOID_REVERSE_CHANNEL);
 
-	// TODO: check what is the module number
-	private Compressor m_phCompressor = new Compressor(Constants.PNEUMATIC_HUB_PORT, PneumaticsModuleType.REVPH);
-	private DoubleSolenoid m_clawSolenoid = new DoubleSolenoid(Constants.DOUBLE_SOLENOID_MODULE_NUMBER, PneumaticsModuleType.REVPH, Constants.DOUBLE_SOLENOID_FORWARD_CHANNEL, Constants.DOUBLE_SOLENOID_REVERSE_CHANNEL);
+    private CANSparkMax m_motor;
+    private RelativeEncoder m_encoder;
+    private double m_speed = 0;
 
-	private CANSparkMax m_motor;
-	private RelativeEncoder m_encoder;
-	private double m_speed = 0;
+    /** Creates a new Claw. */
+    public Claw() {
+        // TODO: which one??
+        // m_phCompressor.enableAnalog(0, 0);
+        m_phCompressor.enableDigital();
+        // TODO: check the port
+        m_motor = new CANSparkMax(0, MotorType.kBrushless);
+        // TODO: should we set brakeMode, etc.?
+        
+        // TODO why do we need to look at the encoder??
+        m_encoder = m_motor.getEncoder();
 
-	public Claw() {
-		// TODO: which one??
-		// m_phCompressor.enableAnalog(0, 0);
-		m_phCompressor.enableDigital();
-		// TODO: check the port
-		m_motor = new CANSparkMax(0, MotorType.kBrushless);
-		m_encoder = m_motor.getEncoder();
+        // TODO: set this conversion factor for RPM
+        m_encoder.setVelocityConversionFactor(0);
+    }
 
-		// TODO: set this conversion factor for RPM
-		m_encoder.setVelocityConversionFactor(0);
-	}
-
-	@Override
-	public void periodic() {
-		if (m_motor.getOutputCurrent() > 10.35) { 
-			m_speed = 0.0;
+    // This method will be called once per scheduler run
+    @Override
+    public void periodic() {
+        // TODO: tune this value. Should be a constant
+        if (m_motor.getOutputCurrent() > 10.35) { 
+            m_speed = 0.0;
         }
-		m_motor.set(m_speed);
-		// This method will be called once per scheduler run
-		SmartDashboard.putBoolean("claw/not full?", m_phCompressor.getPressureSwitchValue());
-		SmartDashboard.putBoolean("claw/isFwdSolenoidDisabled", m_clawSolenoid.isFwdSolenoidDisabled());
-		SmartDashboard.putBoolean("claw/isRevSolenoidDisabled", m_clawSolenoid.isRevSolenoidDisabled());
-	}
+        m_motor.set(m_speed);
 
-	public void toggleClaw(){
-		m_clawSolenoid.toggle();
-	}
+        // TODO: this does not make sense. Testing the compressor does not test if something is in the claw
+        // SmartDashboard.putBoolean("claw/not full?", m_phCompressor.getPressureSwitchValue());
+        SmartDashboard.putBoolean("claw/isFwdSolenoidDisabled", m_clawSolenoid.isFwdSolenoidDisabled());
+        SmartDashboard.putBoolean("claw/isRevSolenoidDisabled", m_clawSolenoid.isRevSolenoidDisabled());
+    }
 
-	public void open(){
-		m_clawSolenoid.set(Value.kForward);
-		m_speed = 0.8;
-	}
+    public void toggleClaw(){
+        m_clawSolenoid.toggle();
+    }
 
-	public void close(){
-		m_clawSolenoid.set(Value.kReverse);
-	}
+    public void open(){
+        m_clawSolenoid.set(Value.kForward);
+        m_speed = 0.8;
+    }
+
+    public void close(){
+        m_clawSolenoid.set(Value.kReverse);
+    }
 }
