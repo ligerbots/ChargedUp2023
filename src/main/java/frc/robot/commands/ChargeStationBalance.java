51b119a3e9555d98463d29b24bc20949383b5f9c
RuihanCao <Ruihan20080129@gmail.com>
2023-02-25 02:38:34 +0800

    added turnSpeed

--- src/main/java/frc/robot/commands/ChargeStationBalance.java
+++ src/main/java/frc/robot/commands/ChargeStationBalance.java
@@ -5,12 +5,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation3d;
 
 public class ChargeStationBalance extends CommandBase {
 
@@ -19,6 +19,8 @@ public class ChargeStationBalance extends CommandBase {
     private static final double BALANCE_KP = 0.02; // change to control how fast robot drives during balancing
     private static final double MAX_MPS = 1.0;
     private static final double BALANCE_SECONDS = 1; // how many seconds the robot has to be balanced before stopping
+    private static final double ANGLE_KP = Units.degreesToRadians(1); 
+    private static final double MAX_ANGLE_SPEED = Units.degreesToRadians(10); 
 
     private DriveTrain m_driveTrain;
     private final Timer m_timer = new Timer();
@@ -47,7 +49,7 @@ public class ChargeStationBalance extends CommandBase {
         // uses angle of robot to set its speed
         double errorDegrees = m_driveTrain.getTiltDegrees() - BALANCED_DEGREES;
         double driveMPS = errorDegrees * BALANCE_KP;
-    
+
         // cap max speed
         if (Math.abs(driveMPS) > MAX_MPS) {
             driveMPS = Math.copySign(MAX_MPS, driveMPS);
@@ -58,11 +60,21 @@ public class ChargeStationBalance extends CommandBase {
         Rotation2d driveAngle = m_driveTrain.getTiltDirection(); //.plus(Rotation2d.fromDegrees(180));
 
         SmartDashboard.putNumber("balanceCommand/driveAngle", driveAngle.getDegrees());
+        double angleError = driveAngle.getRadians();
+        if (Math.abs(angleError) > Math.PI/2) {
+            angleError = angleError - Math.PI;
+        }
+        double angleSpeed = angleError * ANGLE_KP;
+        if (Math.abs(angleSpeed) > MAX_ANGLE_SPEED) {
+            angleSpeed = Math.copySign(MAX_ANGLE_SPEED, angleSpeed);
+        }
+        SmartDashboard.putNumber("balanceCommand/angleSpeed", angleSpeed);
+
 
         ChassisSpeeds chassisSpeeds = new ChassisSpeeds(
             driveMPS * driveAngle.getCos(),
             driveMPS * driveAngle.getSin(),
-            0
+            angleSpeed
         );
 
         m_driveTrain.drive(chassisSpeeds);
