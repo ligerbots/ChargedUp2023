bd8ae600af7d13d4933c848028013c13cf8eb85c
Paul Rensing <prensing@gmail.com>
2023-02-23 19:36:50 -0500

    added enum for color

--- src/main/java/frc/robot/subsystems/LEDLight.java
+++ src/main/java/frc/robot/subsystems/LEDLight.java
@@ -8,6 +8,26 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDLight extends SubsystemBase {
+    public enum Color {
+        OFF(0),
+
+        PURPLE_GREEN(1),
+
+        PURPLE_RED(3),
+
+        YELLOW_GREEN(5),
+
+        YELLOW_RED(7),
+
+        ORANGE(9);
+
+        public int value;
+
+        Color(int val) {
+            this.value = val;
+        }
+    }
+
     /** Creates a new LEDLight. */
     I2C m_led = new I2C(I2C.Port.kOnboard, 0x04);
 
@@ -19,7 +39,7 @@ public class LEDLight extends SubsystemBase {
         // This method will be called once per scheduler run
     }
 
-    public void setColor(int color){
-        m_led.write(0x04, color);
+    public void setColor(Color color) {
+        m_led.write(0x04, color.value);
     }
 }
