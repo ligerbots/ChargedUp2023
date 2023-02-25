adb5622164305eabd3af58aa1a6c27923e8c3d79
Paul Rensing <prensing@gmail.com>
2023-02-25 13:09:21 -0500

    set compressor on in init.

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -4,43 +4,41 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.RelativeEncoder;
 
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticHub;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc.robot.Constants;
 
 public class Claw extends SubsystemBase {
     PneumaticHub m_pH = new PneumaticHub(Constants.PNEUMATIC_HUB_PORT);
     DoubleSolenoid m_clawSolenoid = m_pH.makeDoubleSolenoid(Constants.DOUBLE_SOLENOID_FORWARD_CHANNEL, Constants.DOUBLE_SOLENOID_REVERSE_CHANNEL);
-    private CANSparkMax m_motor;
-    private RelativeEncoder m_encoder;
-    private double m_speed = 0;
+    // private CANSparkMax m_motor;
+    // private RelativeEncoder m_encoder;
+    // private double m_speed = 0;
 
     /** Creates a new Claw. */
     public Claw() {
-        SmartDashboard.putBoolean("claw/isCompressorEnabled", m_pH.getCompressor());
+        SmartDashboard.putBoolean("claw/isCompressorEnabled", true);
     }
 
     // This method will be called once per scheduler run
     @Override
     public void periodic() {
 
-        // TODO: this does not make sense. Testing the compressor does not test if something is in the claw
         // SmartDashboard.putBoolean("claw/not full?", m_phCompressor.getPressureSwitchValue());
         SmartDashboard.putBoolean("claw/isFwdSolenoidDisabled", m_clawSolenoid.isFwdSolenoidDisabled());
         SmartDashboard.putBoolean("claw/isRevSolenoidDisabled", m_clawSolenoid.isRevSolenoidDisabled());
-        if(SmartDashboard.getBoolean("claw/isCompressorEnabled", false))
-            m_pH.enableCompressorDigital();
+        
+        if (SmartDashboard.getBoolean("claw/isCompressorEnabled", false))
+            enableCompressor();
         else
-            m_pH.disableCompressor();
+            disableCompressor();
     }
 
     public void toggleClaw(){
@@ -59,5 +57,9 @@ public class Claw extends SubsystemBase {
 
     public void enableCompressor(){
         m_pH.enableCompressorDigital();
+    }
+
+    public void disableCompressor(){
+        m_pH.disableCompressor();
     }
 }
