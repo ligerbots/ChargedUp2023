f21181905e8f45ae973d11c9ad34b7baa14b709b
shadowdragon2805 <lexue28@gmail.com>
2023-05-25 20:33:41 -0400

    tried to add pneumatic cylinder

--- src/main/java/frc/robot/subsystems/CubeShooter.java
+++ src/main/java/frc/robot/subsystems/CubeShooter.java
@@ -4,18 +4,37 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 
 
 public class CubeShooter {
-    private final CANSparkMax m_cubeShooterMotor;
+    private final CANSparkMax m_motor1;
+    private final CANSparkMax m_motor2;
     DigitalInput m_beamBreak = new DigitalInput(0);
 
+    // pneumatic cylinder
+    PneumaticHub m_pH = new PneumaticHub(Constants.PNEUMATIC_HUB_CUBE_SHOOTER);
+    DoubleSolenoid m_clawSolenoid = m_pH.makeDoubleSolenoid(Constants.DOUBLE_SOLENOID_FORWARD_CHANNEL_CUBE, Constants.DOUBLE_SOLENOID_REVERSE_CHANNEL_CUBE);
 
     public CubeShooter() {
-        // motor for cube shooting
-        m_cubeShooterMotor = new CANSparkMax(Constants.CUBE_SHOOTER_CAN_ID, MotorType.kBrushless);
-        m_cubeShooterMotor.restoreFactoryDefaults();    
+        m_motor1 = new CANSparkMax(Constants.CUBE_SHOOTER_CAN_ID_1, MotorType.kBrushless);
+        m_motor1.restoreFactoryDefaults();    
+        m_motor2 = new CANSparkMax(Constants.CUBE_SHOOTER_CAN_ID_2, MotorType.kBrushless);
+        m_motor2.restoreFactoryDefaults();  
+        SmartDashboard.putBoolean("cubeShooter/isCompressorEnabled", true);
+  
+    }
+
+    public void periodic(){
+        if (SmartDashboard.getBoolean("claw/isCompressorEnabled", true))
+            enableCompressor();
+        else
+            disableCompressor();
+
     }
 
     // is there a clear path between sensors
@@ -24,12 +43,23 @@ public class CubeShooter {
     }
     // intake a cube
     public void startIntake() {
+        m_clawSolenoid.set(Value.kForward);
         //change speed later
-        m_cubeShooterMotor.set(-1.0);
+        m_motor1.set(-1.0);
     }
 
     // shoot a cube by inputting desired speed
     public void shootCube(double shooterSpeed) {
-        m_cubeShooterMotor.set(shooterSpeed);
+        // 2 motors used for shootign cube
+        m_motor1.set(shooterSpeed);
+        m_motor2.set(shooterSpeed);
+
+    }
+    public void enableCompressor() {
+        m_pH.enableCompressorDigital();
+    }
+
+    public void disableCompressor() {
+        m_pH.disableCompressor();
     }
 }
