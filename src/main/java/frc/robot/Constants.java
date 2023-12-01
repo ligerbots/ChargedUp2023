e1dda10db5d6d08a2f32f8332b4e2aa8ea86345a
Paul Rensing <prensing@gmail.com>
2023-11-30 21:01:51 -0500

    Update swerve calib

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,22 +28,22 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(88.9);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(88.2);
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.8);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(359.5);
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(72.2);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(81.8);
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(110.9);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(109.8);
 
     // CAN ID and channels for the Claw subsystem
     public static final int PNEUMATIC_HUB_PORT = 1;
